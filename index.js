52356bd7f1e8e65e3c35dd4c8bdc896fcb1d8e1e
bmdilts <bmdilts@gmail.com>
2017-06-20 22:58:20 -0400

    completed project

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+app.use('/public', express.static('public'));
 
 app.get("/", function(req, res){
   res.render("home", {users: data.users})
+});
+
+app.get("/users/:id", function(req, res){
+  res.render("users", {users: data.users[req.params.id]})
 });
 
 app.listen(3000, function () {
